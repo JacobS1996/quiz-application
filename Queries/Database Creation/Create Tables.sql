bd0052ff3b524e674dffe66c6fd32faa2570e251
Jacob Sherrill <141872571+JacobS1996@users.noreply.github.com>
2025-01-17 14:27:32 -0700

    Update Create Tables.sql
    changed "PercentageNeededToPass" to "PercentageRequiredToPass" in table creation query
    
    Signed-off-by: Jacob Sherrill <141872571+JacobS1996@users.noreply.github.com>

--- Queries/Database Creation/Create Tables.sql
+++ Queries/Database Creation/Create Tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE dbo.Quizzes
 (
 	Id INT NOT NULL PRIMARY KEY IDENTITY,
 	QuizTitle VARCHAR(20) NOT NULL, 
-	PercentageNeededToPass INT NOT NULL
+	PercentageRequiredToPass INT NOT NULL
 );
 
 CREATE TABLE dbo.Questions
